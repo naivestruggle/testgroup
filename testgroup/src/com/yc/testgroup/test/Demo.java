cef344e67117669d5a18aef65d1a756d6f285b47
naivestruggle <naivestruggle@163.com>
2019-04-08 21:30:59 +0800

    123123

--- testgroup/src/com/yc/testgroup/test/Demo.java
+++ testgroup/src/com/yc/testgroup/test/Demo.java
@@ -3,6 +3,7 @@ package com.yc.testgroup.test;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("hello");
-		System.out.println("asjdlasdl");
+		System.out.println("asjdlasdl123123");
+		int a = 0;
 	}
 }
