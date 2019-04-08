87b85d47e4e4efbb1b78ca3056fdebba293b8b2c
naivestruggle <naivestruggle@163.com>
2019-04-08 21:27:51 +0800

    yxh1

--- testgroup/src/com/yc/testgroup/test/Demo.java
+++ testgroup/src/com/yc/testgroup/test/Demo.java
@@ -3,7 +3,6 @@ package com.yc.testgroup.test;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("hello");
-		System.out.println("123");
 		System.out.println("asjdlasdl");
 	}
 }
