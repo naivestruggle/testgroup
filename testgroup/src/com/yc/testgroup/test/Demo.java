ab20c7e04f964642415b6275061b167769bd9f6d
sansheng <807156237@qq.com>
2019-04-08 21:26:13 +0800

    asd

--- testgroup/src/com/yc/testgroup/test/Demo.java
+++ testgroup/src/com/yc/testgroup/test/Demo.java
@@ -2,7 +2,7 @@ package com.yc.testgroup.test;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello");
 		System.out.println("123");
 		System.out.println("asjdlasdl");
 	}
