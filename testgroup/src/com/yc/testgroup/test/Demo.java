8b21f1dec689383dda0ba82f338be26849a67dde
naivestruggle <naivestruggle@163.com>
2019-04-08 21:21:17 +0800

    yyy

--- testgroup/src/com/yc/testgroup/test/Demo.java
+++ testgroup/src/com/yc/testgroup/test/Demo.java
@@ -2,6 +2,7 @@ package com.yc.testgroup.test;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("hello world");s
+		System.out.println("hello world");
+		System.out.println("123");
 	}
 }
