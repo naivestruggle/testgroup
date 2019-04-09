c330f073496a1f82fe20ce2575b7b99825244d33
naivestruggle <naivestruggle@163.com>
2019-04-09 13:58:47 +0800

    2019.4.9 13:58

---
+++ travelers/src/main/webapp/sql/travelers.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : user
+Source Server Version : 50561
+Source Host           : localhost:3306
+Source Database       : travelers
+
+Target Server Type    : MYSQL
+Target Server Version : 50561
+File Encoding         : 65001
+
+Date: 2019-04-09 13:49:07
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
