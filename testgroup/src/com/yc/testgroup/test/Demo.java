4379eea4dcf39071ee59212696c3f6dbfc0e1cff
naivestruggle <naivestruggle@163.com>
2019-04-10 08:59:50 +0800

    second

--- testgroup/src/com/yc/testgroup/test/Demo.java
+++ testgroup/src/com/yc/testgroup/test/Demo.java
@@ -3,7 +3,7 @@ package com.yc.testgroup.test;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("hello");
-		String str = "123123";
+		String str = "1221ÇëÎÊ123";
 		int a = 0;
 	}
 }
